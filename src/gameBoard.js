b5d1f80306d4a45ffd2a7636ab5af3f7d7ee2dcc
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-11-12 02:20:37 +0100

    placeShip method places object with relevant info to the grid

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -8,11 +8,26 @@ export const gameBoardFactory = (gridSize) => {
       grid[i].push('');
     }
   }
-  const placeShip = (shipSize, direction, xCoord, yCoord) => {
-    const ship = shipFactory(shipSize, direction, xCoord, yCoord);
+  const placeShip = (shipSize, direction, yCoord, xCoord) => {
+    const ship = shipFactory(shipSize, direction, yCoord, xCoord);
+    if (ship.direction === 'vertical') {
+      for (let i = 0; i < shipSize; i += 1) {
+        grid[yCoord + i][xCoord] = {
+          status: 'notHit',
+          id: ship.id,
+          yCoord: yCoord + i,
+          xCoord,
+        };
+      }
+    }
     if (ship.direction === 'horizontal') {
       for (let i = 0; i < shipSize; i += 1) {
-        grid[yCoord][xCoord + i] = 'notHit';
+        grid[yCoord][xCoord + i] = {
+          status: 'notHit',
+          id: ship.id,
+          yCoord,
+          xCoord: xCoord + i,
+        };
       }
     }
   };
--- src/gameBoard.test.js
+++ src/gameBoard.test.js
@@ -19,16 +19,16 @@ test('GameBoardFactory generates correct grid size', () => {
 test('gameBoard.placeShip places horizontal ship correctly', () => {
   const gameBoard = gameBoardFactory(10);
   gameBoard.placeShip(3, 'horizontal', 4, 3);
-  expect(gameBoard.grid).toEqual([
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', 'notHit', 'notHit', 'notHit', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', ''],
-  ]);
+  console.log(gameBoard.grid, 'hello');
+  expect(gameBoard.grid[4][3].status).toBe('notHit');
+  expect(gameBoard.grid[4][4].status).toBe('notHit');
+  expect(gameBoard.grid[4][5].status).toBe('notHit');
+});
+
+test('gameBoard.placeShip places horizontal ship correctly', () => {
+  const gameBoard = gameBoardFactory(10);
+  gameBoard.placeShip(3, 'vertical', 4, 1);
+  expect(gameBoard.grid[4][1].status).toBe('notHit');
+  expect(gameBoard.grid[5][1].status).toBe('notHit');
+  expect(gameBoard.grid[6][1].status).toBe('notHit');
 });
