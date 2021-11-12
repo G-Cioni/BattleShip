b2cd4a487c74239e17abdb9e83329eaa762b4c1b
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-11-12 02:30:36 +0100

    merged vertical and horizontal test into one

--- src/gameBoard.test.js
+++ src/gameBoard.test.js
@@ -16,18 +16,14 @@ test('GameBoardFactory generates correct grid size', () => {
   ]);
 });
 
-test('gameBoard.placeShip places horizontal ship correctly', () => {
+test('gameBoard.placeShip places ships correctly', () => {
   const gameBoard = gameBoardFactory(10);
   gameBoard.placeShip(3, 'horizontal', 4, 3);
+  gameBoard.placeShip(3, 'vertical', 4, 1);
   console.log(gameBoard.grid, 'hello');
   expect(gameBoard.grid[4][3].status).toBe('notHit');
   expect(gameBoard.grid[4][4].status).toBe('notHit');
   expect(gameBoard.grid[4][5].status).toBe('notHit');
-});
-
-test('gameBoard.placeShip places horizontal ship correctly', () => {
-  const gameBoard = gameBoardFactory(10);
-  gameBoard.placeShip(3, 'vertical', 4, 1);
   expect(gameBoard.grid[4][1].status).toBe('notHit');
   expect(gameBoard.grid[5][1].status).toBe('notHit');
   expect(gameBoard.grid[6][1].status).toBe('notHit');
