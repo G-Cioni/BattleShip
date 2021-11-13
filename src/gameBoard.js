1d5673dd7e4836f84885f02d9ce9b2baebb3af12
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-11-13 02:48:31 +0100

    removed coordinates from grid tiles

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -12,33 +12,29 @@ export const gameBoardFactory = (gridSize) => {
   const placeShip = (shipSize, direction, yCoord, xCoord) => {
     const ship = shipFactory(shipSize, direction, yCoord, xCoord);
     allShips.push(ship);
-    let allSlotsFree = true;
+    let allTiles = true;
     if (ship.direction === 'vertical' && yCoord + shipSize <= gridSize) {
       for (let i = 0; i < shipSize; i += 1) {
-        allSlotsFree *= grid[yCoord + i][xCoord] === '';
+        allTiles *= grid[yCoord + i][xCoord] === '';
       }
-      if (allSlotsFree) {
+      if (allTiles) {
         for (let i = 0; i < shipSize; i += 1) {
           grid[yCoord + i][xCoord] = {
             status: 'notHit',
             id: ship.id,
-            yCoord: yCoord + i,
-            xCoord,
           };
         }
       }
     }
     if (ship.direction === 'horizontal' && xCoord + shipSize <= gridSize) {
       for (let i = 0; i < shipSize; i += 1) {
-        allSlotsFree *= grid[yCoord][xCoord + i] === '';
+        allTiles *= grid[yCoord][xCoord + i] === '';
       }
-      if (allSlotsFree) {
+      if (allTiles) {
         for (let i = 0; i < shipSize; i += 1) {
           grid[yCoord][xCoord + i] = {
             status: 'notHit',
             id: ship.id,
-            yCoord,
-            xCoord: xCoord + i,
           };
         }
       }
