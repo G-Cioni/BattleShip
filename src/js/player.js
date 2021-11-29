5afeff9e1555d4a3fb24df0f60869a10b2db484b
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-11-29 02:01:36 +0100

    added moveCount

--- src/js/player.js
+++ src/js/player.js
@@ -1,18 +1,22 @@
 import getRandomCoordinates from './getRandomCoordinates';
 
 const playerFactory = (name) => {
+  let moveCount = 0;
   const move = (yCoord, xCoord, gameBoard) => {
     gameBoard.receiveAttack(yCoord, xCoord);
+    moveCount += 1;
   };
-  return { move, name };
+  return { move, moveCount, name };
 };
 
 const cpuPlayerFactory = () => {
+  let moveCount = 0;
   const move = (gameBoard, gridSize) => {
     const { yRandom, xRandom } = getRandomCoordinates(gameBoard, gridSize);
     gameBoard.receiveAttack(yRandom, xRandom);
+    moveCount += 1;
   };
-  return { move };
+  return { move, moveCount };
 };
 
 export { playerFactory, cpuPlayerFactory };
