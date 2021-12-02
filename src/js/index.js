3f02a886b0320632058989ee46ec0b72c241e9dd
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-12-02 02:52:02 +0100

    runGame + renderGameBoard added to index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,19 @@
 import '../reset.css';
 import '../styles.css';
+import renderGameBoard from './dom';
+import { runGame } from './runGame';
+
+const { p1GameBoard, p2GameBoard } = runGame(
+  'human',
+  'cpu',
+  'Player 1',
+  'Player 2',
+  10,
+  1,
+  2,
+  3,
+  4,
+);
+
+renderGameBoard(p1GameBoard, 'player 1');
+renderGameBoard(p2GameBoard, 'player 2');
