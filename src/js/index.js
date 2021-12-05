789d0b7b825868513887f54ccc63acde8fcaa29a
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-12-05 02:01:28 +0100

    temporary changes to run the game in a testing phase

--- src/js/index.js
+++ src/js/index.js
@@ -3,22 +3,23 @@ import '../styles.css';
 // eslint-disable-next-line no-unused-vars
 import { renderNewShips, renderGameBoard } from './dom';
 import { runGame } from './runGame';
-import gameLoop from './gameLoop';
+import { gameLoop } from './gameLoop';
 
+const gridSize = 10;
 const { p1GameBoard, p2GameBoard, player1, player2 } = runGame(
   'human',
-  'human',
+  'cpu',
   'Player 1',
   'Player 2',
-  10,
+  gridSize,
   1,
   2,
   3,
   4,
 );
 
-renderGameBoard(p1GameBoard, player1);
+renderGameBoard(p1GameBoard, player1, p2GameBoard, player2, gridSize);
 renderNewShips(p1GameBoard, player1);
-renderGameBoard(p2GameBoard, player2);
+renderGameBoard(p2GameBoard, player2, p1GameBoard, player1, gridSize);
 
 gameLoop(p1GameBoard, p2GameBoard, player1, player2);
