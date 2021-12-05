eb6ce0c634b27c6891af01f7592155363cd7000d
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-12-05 03:05:13 +0100

    human player moves and then cpu opponent makes move

--- src/tests/gameLoop.test.js
+++ src/tests/gameLoop.test.js
@@ -1,2 +1,31 @@
-import gameLoop from '../js/gameLoop';
-test('', () => {});
+import { runGame } from '../js/runGame';
+import gameLoop, { move } from '../js/gameLoop';
+test('move function lets a human player and then makes cpu opponent move automatically', () => {
+  const gridSize = 10;
+  const { p1GameBoard, p2GameBoard, player1, player2 } = runGame(
+    'cpu',
+    'human',
+    'Player 1',
+    'Player 2',
+    gridSize,
+    1,
+    2,
+    3,
+    4,
+  );
+  move(p1GameBoard, player1, p2GameBoard, player2, gridSize, 0, 0);
+  const p2GameBoardHit = p2GameBoard.grid.reduce((acc, row) => {
+    row.forEach((tile) => {
+      if (tile === 'missed' || tile.status === 'hit') {
+        console.log(tile);
+        acc = true;
+      }
+    });
+    return acc;
+  }, false);
+  expect(
+    p1GameBoard.grid[0][0] === 'missed' ||
+      p1GameBoard.grid[0][0].status === 'hit',
+  ).toBe(true);
+  expect(p2GameBoardHit).toBe(true);
+});
