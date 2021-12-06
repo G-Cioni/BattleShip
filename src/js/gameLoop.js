32947e454a6408fce84d57983605b136cc90a988
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-12-06 02:31:23 +0100

    fixed inversion of x and y coordinates

--- src/js/gameLoop.js
+++ src/js/gameLoop.js
@@ -4,18 +4,18 @@ const move = (
   opponentGameBoard,
   opponent,
   gridSize,
-  xCoord,
   yCoord,
+  xCoord,
 ) => {
   //! moveCount doesn't increase for some reason
   //! maybe because player is locked to the gameBoard when created with 0 moveCount and its not referenced properly (might have to do with "this" although it shouldn't because I'm not working with classes)
 
   if (
-    gameBoard.grid[xCoord][yCoord] === '' ||
-    gameBoard.grid[xCoord][yCoord].status === 'notHit'
+    gameBoard.grid[yCoord][xCoord] === '' ||
+    gameBoard.grid[yCoord][xCoord].status === 'notHit'
   ) {
     if (opponent.type === 'human' && opponent.moveCount <= player.moveCount) {
-      opponent.move(xCoord, yCoord, gameBoard);
+      opponent.move(yCoord, xCoord, gameBoard);
     }
     if (player.type === 'cpu') {
       player.move(opponentGameBoard, gridSize);
