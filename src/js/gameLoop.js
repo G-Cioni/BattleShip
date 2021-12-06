a6698765b67b21ac9195c66ccf3b645fd35e8dfe
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-12-06 03:52:04 +0100

    moveCount is now working

--- src/js/gameLoop.js
+++ src/js/gameLoop.js
@@ -7,18 +7,19 @@ const move = (
   yCoord,
   xCoord,
 ) => {
-  //! moveCount doesn't increase for some reason
-  //! maybe because player is locked to the gameBoard when created with 0 moveCount and its not referenced properly (might have to do with "this" although it shouldn't because I'm not working with classes)
-
   if (
     gameBoard.grid[yCoord][xCoord] === '' ||
     gameBoard.grid[yCoord][xCoord].status === 'notHit'
   ) {
     if (opponent.type === 'human' && opponent.moveCount <= player.moveCount) {
       opponent.move(yCoord, xCoord, gameBoard);
+      // eslint-disable-next-line no-param-reassign
+      opponent.moveCount += 1;
     }
-    if (player.type === 'cpu') {
+    if (player.type === 'cpu' && player.moveCount <= opponent.moveCount) {
       player.move(opponentGameBoard, gridSize);
+      // eslint-disable-next-line no-param-reassign
+      player.moveCount += 1;
     }
   }
 };
--- src/js/player.js
+++ src/js/player.js
@@ -2,25 +2,21 @@ import { getRandomCoordinates } from './utils';
 
 // Creates human player
 const playerFactory = (name, number) => {
-  let moveCount = 0;
+  const moveCount = 0;
   const type = 'human';
-  const move = (yCoord, xCoord, gameBoard) => {
-    const successfulAttack = gameBoard.receiveAttack(yCoord, xCoord);
-    if (successfulAttack) {
-      moveCount += 1;
-    }
-  };
+  const move = (yCoord, xCoord, gameBoard) =>
+    gameBoard.receiveAttack(yCoord, xCoord);
+
   return { move, moveCount, name, number, type };
 };
 
 // Creates cpu player
 const cpuPlayerFactory = (number) => {
-  let moveCount = 0;
+  const moveCount = 0;
   const type = 'cpu';
   const move = (gameBoard, gridSize) => {
     const { yRandom, xRandom } = getRandomCoordinates(gameBoard, gridSize);
     gameBoard.receiveAttack(yRandom, xRandom);
-    moveCount += 1;
   };
   return { move, moveCount, number, type };
 };
