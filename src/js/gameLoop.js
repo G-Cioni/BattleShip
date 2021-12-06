1f65591c427669ca72c7c51131d1177b09c57d5e
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-12-06 01:54:43 +0100

    incorporated moveIfPossible logic into move function

--- src/js/gameLoop.js
+++ src/js/gameLoop.js
@@ -9,28 +9,20 @@ const move = (
 ) => {
   //! moveCount doesn't increase for some reason
   //! maybe because player is locked to the gameBoard when created with 0 moveCount and its not referenced properly (might have to do with "this" although it shouldn't because I'm not working with classes)
-  if (opponent.type === 'human' && opponent.moveCount <= player.moveCount) {
-    opponent.move(xCoord, yCoord, gameBoard);
-  }
-  if (player.type === 'cpu') {
-    player.move(opponentGameBoard, gridSize);
-  }
-};
 
-const moveIfPossible = (
-  gameBoard,
-  player,
-  opponentGameBoard,
-  opponent,
-  gridSize,
-  i,
-  j,
-) => {
-  if (gameBoard.grid[i][j] === '' || gameBoard.grid[i][j].status === 'notHit') {
-    move(gameBoard, player, opponentGameBoard, opponent, gridSize, i, j);
+  if (
+    gameBoard.grid[xCoord][yCoord] === '' ||
+    gameBoard.grid[xCoord][yCoord].status === 'notHit'
+  ) {
+    if (opponent.type === 'human' && opponent.moveCount <= player.moveCount) {
+      opponent.move(xCoord, yCoord, gameBoard);
+    }
+    if (player.type === 'cpu') {
+      player.move(opponentGameBoard, gridSize);
+    }
   }
 };
 
 const gameLoop = () => {};
 
-export { moveIfPossible, gameLoop };
+export { move, gameLoop };
