7fbe8beb892a610449ac83d303d6c7941c89cdbe
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-12-09 03:18:58 +0100

    added basic AI to the cpu

--- src/js/player.js
+++ src/js/player.js
@@ -15,8 +15,54 @@ const cpuPlayerFactory = (name, number) => {
   const moveCount = 0;
   const type = 'cpu';
   const move = (gameBoard, gridSize) => {
-    const { yRandom, xRandom } = getRandomCoordinates(gameBoard.grid, gridSize);
-    gameBoard.receiveAttack(yRandom, xRandom);
+    // If a tile next to a tile which has been hit is free then it is added to goodMoves array
+    const goodMoves = [];
+    for (let i = 0; i < gameBoard.grid.length; i += 1) {
+      for (let j = 0; j < gameBoard.grid.length; j += 1) {
+        if (gameBoard.grid[i][j].status === 'hit') {
+          if (
+            i > 0 &&
+            (gameBoard.grid[i - 1][j] === '' ||
+              gameBoard.grid[i - 1][j].status === 'notHit')
+          ) {
+            goodMoves.push([i - 1, j]);
+          }
+          if (
+            i < gameBoard.grid.length - 1 &&
+            (gameBoard.grid[i + 1][j] === '' ||
+              gameBoard.grid[i + 1][j].status === 'notHit')
+          ) {
+            goodMoves.push([i + 1, j]);
+          }
+          if (
+            j > 0 &&
+            (gameBoard.grid[i][j - 1] === '' ||
+              gameBoard.grid[i][j - 1].status === 'notHit')
+          ) {
+            goodMoves.push([i, j - 1]);
+          }
+          if (
+            j < gameBoard.grid[i].length - 1 &&
+            (gameBoard.grid[i][j + 1] === '' ||
+              gameBoard.grid[i][j + 1].status === 'notHit')
+          ) {
+            goodMoves.push([i, j + 1]);
+          }
+        }
+      }
+    }
+    // If available makes an attack with a goodMove
+    if (goodMoves.length > 0) {
+      const random = Math.floor(Math.random() * goodMoves.length);
+      const [y, x] = goodMoves[random];
+      gameBoard.receiveAttack(y, x);
+    } else {
+      const { yRandom, xRandom } = getRandomCoordinates(
+        gameBoard.grid,
+        gridSize,
+      );
+      gameBoard.receiveAttack(yRandom, xRandom);
+    }
   };
   return { move, moveCount, name, number, type };
 };
